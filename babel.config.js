1a6ffd4f25672e6b505ecde2b10a6110ca8b3449
Gabriel Pires <gabpiresdev@gmail.com>
2023-12-18 20:58:08 -0300

    🧹 chore(project): setup project with nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@utils': './src/utils',
+            '@assets': './src/assets',
+            '@constants': './src/constants',
+            '@hooks': './src/hooks',
+            '@types': './src/types',
+            '@services': './src/services',
+            '@theme': './src/theme',
+            '@config': './src/config',
+          },
+        },
+      ],
+    ],
+  }
+}
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native'
+
+export function SignInScreen() {
+  return <View className="flex-1 bg-zinc-900"></View>
+}
