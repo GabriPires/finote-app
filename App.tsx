53bac7eaec18533e28199fb4e156ec90e03499d7
Gabriel Pires <gabpiresdev@gmail.com>
2023-12-18 21:06:37 -0300

    ✨ feat(sign-in): start sign in page

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import { SignInScreen } from './src/screens/SignIn'
 
 export default function App() {
   return (
-    <>
+    <View className="flex-1 bg-zinc-50 dark:bg-zinc-900">
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignInScreen />
-    </>
+    </View>
   )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,15 @@
-import { View } from 'react-native'
+import { GoogleLogo } from 'phosphor-react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 export function SignInScreen() {
-  return <View className="flex-1 bg-zinc-900"></View>
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Vamos começar?</Text>
+
+      <TouchableOpacity>
+        <GoogleLogo />
+        <Text>Entrar com o Google</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
