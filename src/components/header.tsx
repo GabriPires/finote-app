06b5eec323c4073150014c85a4ec18ae8114c752
GabriPires <gabpiresdev@gmail.com>
2024-02-08 14:46:35 -0300

    ♻️ refactor(header): remove example avatar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
-import { SignOut } from 'phosphor-react-native'
-import { Image, View } from 'react-native'
+import { SignOut, User } from 'phosphor-react-native'
+import { View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import colors from 'tailwindcss/colors'
 
 import { supabase } from '@/lib/supabase'
 
@@ -21,11 +22,18 @@ export function Header() {
         paddingTop,
       }}
     >
-      <View className="overflow-hidden rounded-full border-2  border-zinc-700">
-        <Image
+      <View className="relative h-11 w-11 overflow-hidden rounded-full border-2 border-zinc-700">
+        {/* <Image
           source={{ uri: 'https://github.com/GabriPires.png' }}
           alt="Foto de perfil do usuário"
-          className="h-10 w-10"
+          className="z-[1] h-10 w-10"
+        /> */}
+        <User
+          size={40}
+          color={colors.zinc[700]}
+          style={{
+            position: 'absolute',
+          }}
         />
       </View>
 
